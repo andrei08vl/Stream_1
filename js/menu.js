b0689d8addf86fbc84bdcb52e627f88db8f5cc52
Andrei <andrei08vl@hotmail.com>
2018-02-04 16:54:55 +0000

    Add menu.js

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	//the news pages is loaded first
+    $("#pages").load("templates/news.html");
+	//when the id is clicked page is loaded in #pages
+    $("#news").click(function(){
+        $("#pages").load("templates/news.html");
+    });
+	 $("#concert").click(function(){
+        $("#pages").load("templates/concerts.html");
+    });
+     $("#media").click(function(){
+        $("#pages").load("templates/media.html");
+    });
+	 $("#contact").click(function(){
+        $("#pages").load("templates/contact.html");
+    });
+});
