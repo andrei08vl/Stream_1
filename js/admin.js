fa360b0c6ce3677bd23c796e6f19b7d7baf452eb
Andrei <andrei08vl@hotmail.com>
2018-02-05 19:28:37 +0000

    Update admin.js with check_admin function

--- js/admin.js
+++ js/admin.js
@@ -6,3 +6,21 @@ function logout (){
     /*Login page redirection*/    
    window.location.assign("../JB_admin.html");
 }
+
+function check_admin(){
+	/* if someone attempts to hit the page without authentication will be automatically redirected 
+	e.g domainname/templates/admin_page*/
+	 var user = {admin:"test", password:"test"};
+     var admin=localStorage.getItem("username");
+     var password=localStorage.getItem("password");
+    if(user.admin==admin && user.password==password){  
+	/* If the condition is true the admin will be welcome */
+      document.getElementById("admin").innerHTML="Welcome: "+admin; 
+     
+}else{
+	/* else will be redirected to login page */
+     window.location.assign("../JB_admin.html");
+}
+
+}
+check_admin();
