4622beca4b1bdae6c55dea04acf33e513ababab6
Andrei <andrei08vl@hotmail.com>
2018-02-05 15:02:25 +0000

    update script function with redirect link

--- js/script.js
+++ js/script.js
@@ -2,22 +2,25 @@
 function script(){
     var admin = document.getElementById('admin');
     var password = document.getElementById('password');
-  
+	var msg="ussername or password are incorect";
     var user = {admin:"test", password:"test"};
     
  
 if(admin.value==user.admin) {
     if(password.value==user.password){
-		alert("successful login");
+		
+		/*window.location.assign redirect to specific link*/
+	 window.location.assign("templates/admin_news.html");
 }
 else{
+	
    alert("ussername or password are incorect");
-    
+     
 }
 }
 else{
-     alert("ussername or password are incorect");
-
+    alert("ussername or password are incorect");
+	
 }
 }
 
