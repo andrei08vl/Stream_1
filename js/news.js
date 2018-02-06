9768306da4b35fc39fac3cff02400cc17119535e
Andrei <andrei08vl@hotmail.com>
2018-02-06 21:13:56 +0000

    add post_news function for news.html

---
+++ js/news.js
@@ -0,0 +1,15 @@
+function post_news(){
+    
+      var news=JSON.parse(localStorage.getItem('obj'));
+      
+        var n;
+        var txt=" ";
+        for(n in news){
+        txt += "<div><h1>"+news[n].title+"</h1><br/>";
+        txt += "<p>"+news[n].date+"</p><br>";
+        txt += "<p>"+news[n].message+"</p></div>";
+    }
+      document.getElementById("nws").innerHTML =txt;
+    
+}
+post_news();
