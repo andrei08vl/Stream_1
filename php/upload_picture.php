95ba8623092d820955c52502a883df3b035537da
Andrei <andrei08vl@hotmail.com>
2018-02-07 14:00:17 +0000

    Update the upload_picture and upload_video files

--- php/upload_picture.php
+++ php/upload_picture.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "../file/pictures/";
+$target_dir = "../files/pictures/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
 ?>
