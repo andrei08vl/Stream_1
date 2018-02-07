da3a9f01b60e98f087a7725946a4cd6a66e97f50
Andrei <andrei08vl@hotmail.com>
2018-02-07 14:55:36 +0000

    Update news.js file

--- php/upload_video.php
+++ php/upload_video.php
@@ -2,4 +2,7 @@
 $target_dir = "../files/videos/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+/* After the file has been sent to the server, the user is automatically redirected to the home page */
+header("Location: http://".$_SERVER['HTTP_HOST']."/templates/admin.html",true,303);
+exit;
 ?>
