49f785f30dcb17cee005efdbe0f212089df3b561
Andrei <andrei08vl@hotmail.com>
2018-02-08 15:20:01 +0000

    Remove load news_function from news.js

--- js/news.js
+++ js/news.js
@@ -14,20 +14,3 @@ function post_news(){
 }
 post_news();
 
-function load_news() {
-	/* XMLHttpRequest is used to send and recive data from server
-		To send data si used Post method and Get to recive data*/
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-	  /*readyState==4 means request finished and response is ready 
-		status == 200 means this is the standard response for successful HTTP requests*/
-    if (this.readyState == 4 && this.status == 200) {
-		/* Data is stored in local storage */
-       localStorage.setItem('obj',JSON.parse(this.responseText));
-     
-    }
-  };
-  xhttp.open("GET", "../file/news.js", true);
-  xhttp.send();
-}
-load_news();
