43a3d9d0977ce320746f47bb0588c40ec1ebfb95
Andrei <andrei08vl@hotmail.com>
2018-02-08 09:47:22 +0000

    Creating the function add_events

--- js/admin_plans.js
+++ js/admin_plans.js
@@ -2,6 +2,8 @@
 
 var events=[{event_type: "event_type", event_date: "event_date", event_time: "event_time", event_description: "event_description"}];
 
+events=localStorage.getItem('events');
+
 function show_events(){
       
 	var e; 
@@ -21,3 +23,29 @@ function show_events(){
 }
 
 show_events();
+
+function add_event() {
+	if(events.value===null){
+		/* If events is null we need to set localStorage as an object '[]' */
+		localStorage.setItem('events','[]');
+	}else{
+		/* Else events will be equal with localStorage data */
+		events=JSON.parse(localStorage.getItem('events'));
+	}
+	var evnt={};
+		/* the information submitted by the form is stored in an array */
+		evnt.event_type=document.getElementById('event_type').value;
+		evnt.event_date=document.getElementById('event_date').value;
+		evnt.event_time=document.getElementById('event_time').value;
+		evnt.event_description=document.getElementById('event_description').value;
+	
+    events.unshift(evnt);
+	/*Update the news_dashboard data without refreshing the page*/
+	show_events();
+	
+  
+	/* Data is stored after in the html5 localStorage */
+	localStorage.setItem('events',JSON.stringify(events));
+
+
+}
