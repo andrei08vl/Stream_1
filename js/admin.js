befbdcee8789b65fb222bba22983d9472a4bbfe9
Andrei <andrei08vl@hotmail.com>
2018-02-09 20:28:55 +0000

    Add load_db function to update localStorage with fresh data and update check_admin function

--- js/admin.js
+++ js/admin.js
@@ -15,7 +15,13 @@ function check_admin(){
      var password=localStorage.getItem("password");
     if(user.admin==admin && user.password==password){  
 	/* If the condition is true the admin will be welcome */
-      document.getElementById("admin").innerHTML="Welcome: "+admin; 
+      document.getElementById("admin").innerHTML="Welcome: "+admin;
+
+	/* If we are logged localStorage will update automatically */
+	load_db("../files/news.js","obj");
+	load_db("../files/pictures.js","pictures");
+	load_db("../files/videos.js","videos");
+	load_db("../files/events.js","events");	  
      
 }else{
 	/* else will be redirected to login page */
@@ -24,3 +30,22 @@ function check_admin(){
 
 }
 check_admin();
+
+function load_db(location,lS_key) {
+	/* 'location' - is path location of server storage files 
+	and 'lS_key' - represent localStorage key
+	    XMLHttpRequest is used to send and recive data from server
+		To send data si used Post method and Get to recive data*/
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	/*readyState==4 means request finished and response is ready 
+	 status == 200 means this is the standard response for successful HTTP requests*/
+    if (this.readyState == 4 && this.status == 200) {
+		/* Data is stored in local storage */
+       localStorage.setItem(lS_key,JSON.parse(this.responseText));
+     
+    }
+  };
+  xhttp.open("GET", location, true);
+  xhttp.send();
+}
