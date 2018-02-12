2d654110246dce4d1ce0674cc8470e7b67719c77
Andrei <andrei08vl@hotmail.com>
2018-02-12 10:24:24 +0000

    Update events.js script file

--- js/events.js
+++ js/events.js
@@ -6,10 +6,10 @@ function post_events(){
 	
 	var events=" ";
 	
-	for(e in events){
+	for(e in post_ev){
 		
-		events += "<div><h1>"+events[e].event_type+"</h1><p>"+events[e].event_description+"</p><br/>";
-		events += "<p><span>"+events[e].event_date+"</span>-"+event[e].event_time+"</p>div>";
+		events += "<div><h1>"+post_ev[e].event_type+"</h1><p>"+post_ev[e].event_description+"</p><br/>";
+		events += "<p><span>"+post_ev[e].event_date+"</span>-"+post_ev[e].event_time+"</p>div>";
 		
 		
 		
