5a0cb15ddc24db0e4b34d977763ae85a73917d17
Andrei <andrei08vl@hotmail.com>
2018-02-12 11:29:05 +0000

    Update the video_pictures.js script page to post the img

--- js/videos_pictures.js
+++ js/videos_pictures.js
@@ -1,3 +1,22 @@
+   /* pictures */
+    var pictures=JSON.parse(localStorage.getItem("pictures"));
+    var p; 
+    var img=" ";
+   
+    img+="<div id='box_media'>";
+    for(p in pictures){
+   
+    
+    img+="<div class='box_img'>";
+   
+    img+="<img class='img_admin' width='100%' src='../files/pictures/"+pictures[p].picture_name+"' alt='"+pictures[p].picture_name+"'/>";
+	
+    img+="</div>";
+    }
+    img+="</div>";
+	/* When you access the page, the pictures are displayed first */
+    document.getElementById('JB_media').innerHTML=img;
+  
   function show_hide(content){
 	  
     if(content==pictures){
@@ -12,5 +31,3 @@
     }
 }
 
-var videos="<p>video</p>";
-var pictures="<p>pictures</p>";
