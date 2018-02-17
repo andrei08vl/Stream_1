1bf9fc0135b673d9caf512dcabca9c6a62ba7afd
Andrei <andrei08vl@hotmail.com>
2018-02-17 21:39:26 +0000

    Update first_slide function

--- js/carusel.js
+++ js/carusel.js
@@ -4,13 +4,24 @@ var pictures=JSON.parse(localStorage.getItem('pictures'));
 function first_slide(i){
  	
 	var carusel=" ";
+	
+	/* For the first picture just right arrow */
+	if(i===0){
+		
+	carusel+="<img src='../files/pictures/"+pictures[i].picture_name+"' width='100%'>";
+	carusel+="<a href='#' class='right carousel-control'><span onclick='slider("+i+"+1)' ";
+	carusel+="class='glyphicon glyphicon-chevron-right'></span></a>";
+		
+	}
+		else{
 
 	carusel+="<img src='../files/pictures/"+pictures[i].picture_name+"' width='100%'>";
 	carusel+="<a href='#' class='right carousel-control'><span onclick='slider("+i+"+1)' ";
 	carusel+="class='glyphicon glyphicon-chevron-right'></span></a>";
 	carusel+="<a href='#' class='left carousel-control'><span onclick='slider("+i+"-1)' ";
 	carusel+="class='glyphicon glyphicon-chevron-left'></span></a>";
-
+	}
+		
 	document.getElementById("carusel").innerHTML=carusel;
 
 }
