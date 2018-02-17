6fb6796a2bd22aa76b370047557683a43e30246c
Andrei <andrei08vl@hotmail.com>
2018-02-17 21:30:42 +0000

    Add slider js function

--- js/carusel.js
+++ js/carusel.js
@@ -3,10 +3,22 @@ var pictures=JSON.parse(localStorage.getItem('pictures'));
 /* When a picture is clicked it will be the first picture from modal slider */
 function first_slide(i){
  	
-var carusel=" ";
+	var carusel=" ";
 
-carusel+="<img src='../files/pictures/"+pictures[i].picture_name+"' width='100%'>";
+	carusel+="<img src='../files/pictures/"+pictures[i].picture_name+"' width='100%'>";
 
-document.getElementById("carusel").innerHTML=carusel;
+	document.getElementById("carusel").innerHTML=carusel;
 
+}
+
+function slider(i){
+	
+	if(i < pictures.length){
+	   carusel(i);
+	
+	}
+	/* When 'i' it's bigger than pictures length will reset */
+	else{
+	carusel(0);
+	}
 }
