23eabeda64cca1bd70f74529bc37c9a9a354c63b
Andrei <andrei08vl@hotmail.com>
2018-02-25 18:24:15 +0000

    Update slide_head_img() function to change the text color depending on the background

--- js/top_picture.js
+++ js/top_picture.js
@@ -18,8 +18,15 @@ function slide_head_img(){
 	/*For pictures with different names we can use an array or object
 	and post it the value*/
 	document.getElementById("JB_top_picture").src ="img/jonas_brothers_top"+i+".jpg";
-	/*Adjust the background for each picture*/
-	var background=["#9d9797","white","black","#110706","#242828"];
+	/*Adjust the background and text color for each picture*/
+	var background=[{color:"#9d9797",text:"black"},
+	                {color:"white",text:"black"},
+	                {color:"black", text:"#009688"},
+	                {color:"#110706", text:"#e5dcdc"},
+	                {color:"#242828", text:"black"}];
+	
+    document.body.style.backgroundColor=background[i].color;
+    document.body.style.color=background[i].text;
 	
     document.body.style.backgroundColor =background[i];
 	// Run function every 60000 miliseconds(1 minutes)
