beb9ed046e1fa9db1ac862d4b0ee5cf41e6fc5b3
Andrei <andrei08vl@hotmail.com>
2018-02-25 17:26:15 +0000

    Update post_events js function

--- js/events.js
+++ js/events.js
@@ -9,7 +9,7 @@ function post_events(){
 	for(e in post_ev){
 		
 		events += "<div><h1>"+post_ev[e].event_type+"</h1><p>"+post_ev[e].event_description+"</p><br/>";
-		events += "<p><span>"+post_ev[e].event_date+"</span>-"+post_ev[e].event_time+"</p><div>";
+		events += "<p><span>"+post_ev[e].event_date+"</span>-"+post_ev[e].event_time+"</p></div>";
 		
 		
 		
